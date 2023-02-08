e5cbb17acb64979e669e452a043b62c7feb388d7
Guillermo <g.ggrao@upm.es>
2023-02-08 17:24:38 +0100

    feat: get_df function implemented

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+import pandas as pd
+
+import numpy as np
+from numpy.random import default_rng
+
+import logging
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    print(get_df())
     return "<p>Hello, World!</p>"
+
+def get_df():
+    a = default_rng(100).random((1000))*100
+    b = a.copy() % 10
+    df = pd.DataFrame({"a": a, "b": b})
+    return df
+
+app.run(debug=True)
