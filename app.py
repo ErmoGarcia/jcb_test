479bab6b58ca7156caf79b99696a5f8f1e43407a
Guillermo <g.ggrao@upm.es>
2023-02-09 17:42:31 +0100

    wip: OpenAPI support

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from apispec import APISpec
 import pandas as pd
 
 import numpy as np
@@ -9,14 +10,51 @@ import logging
 app = Flask(__name__)
 rng = default_rng(100)
 
+spec = APISpec(
+    title = 'JCB Web App',
+    version = '1.0.0',
+    openapi_version = '3.1.0'
+)
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/data")
 def json_data():
+    """
+    Get data in json format
+    ---
+    get:
+        description: Get randomly generated data
+        response:
+            200:
+                description: Return data consisting of two columns. Column a consists of random numbers in the 0-100 range. Column b consistsof column a data modulo 10.
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                a:
+                                    type: object
+                                    additionalProperties:
+                                        type: double
+                                        minimum: 0
+                                        maximum: 100
+                                b:
+                                    type: object
+                                    additionalProperties:
+                                        type: double
+                                        minimum: 0
+                                        maximum: 10
+    ---
+    """
     df = get_df()
     return df.to_json()
+
+@app.route("/api/swagger.json")
+def swagger_spec():
+    return jsonify(spec.to_dict())
 
 def get_df():
     a = rng.random((1000))*100
@@ -24,4 +62,5 @@ def get_df():
     df = pd.DataFrame({"a": a, "b": b})
     return df
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
